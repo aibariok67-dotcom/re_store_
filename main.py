6162b0e60a1568a4a4491e39f386af58afe1b9cf
Aibar <aibariok67@gmail.com>
2026-03-13 17:14:32 +0500

    feat: add dotenv support, fix migration file

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI(title="RE Store", version="0.2")
 
 app.include_router(games.router)
 app.include_router(categories.router)
-app.include_router(platforms.router)
+app.include_router(platforms.router) 
